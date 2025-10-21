3617e2053666f82ba3e396ffd7cd525225bc7649
Admin <vadimsuman55@gmail.com>
2025-10-21 14:58:59 +0700

    Demoscreen

--- app/src/main/java/com/example/tip_calc/MainActivity.kt
+++ app/src/main/java/com/example/tip_calc/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.tip_calc.ui.theme.Tip_CalcTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Tip_CalcTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    DemoScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
-
+@Preview(showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun DemoTextPreview() {
     Tip_CalcTheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            DemoScreen(modifier = Modifier.padding(innerPadding))
+        }
+    }
+}
+@Composable
+fun DemoScreen(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Калькулятор скидок и чаевых",
+            fontSize = 24.sp,
+            style = MaterialTheme.typography.headlineMedium
+        )
     }
 }
